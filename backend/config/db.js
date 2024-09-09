24adf48e518efb8fdf2ecbd056284b529208dda7
SolomonAsiya <solasiya@gmail.com>
2024-09-09 17:42:29 +0200

    new_ups

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 
 // Create connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 // Establish the connection to the database
