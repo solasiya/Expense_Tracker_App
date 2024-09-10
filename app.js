a20bd6c23f03d9d0d6c5bf0ac1902fcf74cb275d
kevin-murithi <murithikevin54@gmail.com>
2024-09-10 12:57:09 +0300

    commit

--- app.js
+++ app.js
@@ -128,10 +128,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-const port = process.env.MYSQLPORT|| 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    debug(`Server running at http://localhost:${port}`);
+  console.log(`App listening on port: ${port}`);
 });
 
 
