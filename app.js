8323b87ec594e37ea83b10abf6070daf5f4c4936
SolomonAsiya <solasiya@gmail.com>
2024-09-10 23:15:09 +0200

    update

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('Server started on port 8080');
 });
 
