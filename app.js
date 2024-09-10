718a8a2464dc33e70a466deb4b8a9894188d1876
SolomonAsiya <solasiya@gmail.com>
2024-09-10 23:14:23 +0200

    update

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(8080, () => {
     console.log('Server started on port 8080');
 });
