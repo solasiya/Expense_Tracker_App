70cfd6d6ab230d035359bdbce8fc5b2b56f97461
kevin-murithi <murithikevin54@gmail.com>
2024-09-10 12:31:29 +0300

    commit

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-const port = 34196 || 3000;
+const port = 5000;
 
 app.listen(port, () => {
     debug(`Server running at http://localhost:${port}`);
