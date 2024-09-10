05fad9e85fd2296790438cc35befb845b4312191
SolomonAsiya <solasiya@gmail.com>
2024-09-10 22:22:16 +0200

    update

--- app.js
+++ app.js
@@ -129,9 +129,8 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.MYSQLPORT || 3000;
-
-app.listen(port, () => {
-  console.log(`App listening on port: ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started on port ${port}`);
 });
 
 
